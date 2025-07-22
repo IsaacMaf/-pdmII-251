95d7257adeff3717d276b9ebe3a4ff47ab9ca819
isaac <isaac.magalhaes07@aluno.ifce.edu.br>
2025-07-22 18:15:36 -0300

    finalizado avaliacao-08

---
+++ avaliacoesTv/avaliacoes/avaliacao-08/avaliacao8/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'user_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista de Usuários',
+      home: UserListPage(),
+    );
+  }
+}
+
+class UserListPage extends StatefulWidget {
+  @override
+  _UserListPageState createState() => _UserListPageState();
+}
+
+class _UserListPageState extends State<UserListPage> {
+  late Future<List<User>> futureUsers;
+
+  Future<List<User>> fetchUsers() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Falha ao carregar usuários');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureUsers = fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Usuários')),
+      body: FutureBuilder<List<User>>(
+        future: futureUsers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('Nenhum usuário encontrado.'));
+          } else {
+            final users = snapshot.data!;
+            return RefreshIndicator(
+              onRefresh: () async {
+                setState(() {
+                  futureUsers = fetchUsers();
+                });
+              },
+              child: ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  return ListTile(
+                    leading: CircleAvatar(child: Text(user.id.toString())),
+                    title: Text(user.name),
+                    subtitle: Text(user.email),
+                  );
+                },
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
